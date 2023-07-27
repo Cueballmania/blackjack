77d791848216390c86a4de4ac75af420e04838c0
Brant A <baabeln@ucdavis.edu>
2023-07-27 08:43:48 -0500

    dealopeninghands done

--- src/OpeningDeal.hs
+++ src/OpeningDeal.hs
@@ -11,17 +11,18 @@ dealOpeningHands = do
     g <- get
     let ps = players g
     let d = dealer g
-    -- for each player, access the active hands
-    -- for each active hand, draw a card
-    -- return each player with a card added to each active hand keeping the original bet
-    newPlayers <- forM ps $ \p -> do
+
+    newPlayers <- dealCardToAllPlayers ps
+    newCard <- drawCard
+    newPlayers2 <- dealCardToAllPlayers newPlayers
+    newCard2 <- drawCard
+    let newDealer2 = d { hand = [newCard], hiddenHand = [newCard2] }
+    put $ g { players = newPlayers2, dealer = newDealer2 }
+
+dealCardToAllPlayers :: [Player] -> State Game [Player]
+dealCardToAllPlayers ps = do
+    forM ps $ \p -> do
         newHandBets <- forM (activeHands p) $ \(h, b) -> do
             newCard <- drawCard
             return (h ++ [newCard], b)
         return p { activeHands = newHandBets }
-    put $ g { players = newPlayers }
-
-    
-    
-
-
