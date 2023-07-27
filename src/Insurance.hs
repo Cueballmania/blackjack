d3911c9d895076b791ed75d89ba9a060511d1ad8
Brant A <baabeln@ucdavis.edu>
2023-07-27 11:11:57 -0500

    Validate Insurance Prompt

--- src/Insurance.hs
+++ src/Insurance.hs
@@ -1,6 +1,6 @@
 module Insurance where
 
-import Types (Dealer(..))
+import Types (Dealer(..), Player (..))
 import Deck (cardValue)
 
 dealerBlackjack :: Dealer -> Bool
@@ -8,3 +8,24 @@ dealerBlackjack d = length h == 2 && handValue == 21
     where
         h = hand d ++ hiddenHand d
         handValue = sum $ map cardValue h
+
+calcMaxInsurance :: Player -> Int
+calcMaxInsurance p = min (bankroll p) (div (sum $ map snd $ activeHands p) 2)
+
+getValidInsuranceBet :: Int -> IO Int
+getValidInsuranceBet maxBet = do
+    putStrLn "Would you like to buy insurance? (Y/N)"
+    response <- getLine
+    case response of
+        "Y" -> do
+            putStrLn $ "You can bet up to" ++ show maxBet ++ "How much would you like to bet?"
+            bet <- read <$> getLine
+            if bet > maxBet
+                then do
+                    putStrLn $ "You can't bet more than " ++ show maxBet
+                    getValidInsuranceBet maxBet
+                else return $ max bet 0
+        "N" -> return 0
+        _ -> do
+            putStrLn "Invalid response. Try again."
+            getValidInsuranceBet maxBet
