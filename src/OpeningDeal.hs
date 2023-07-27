c85dec95f2d8424dc44036351bd70c51caef2c28
Brant A <baabeln@ucdavis.edu>
2023-07-27 08:20:36 -0500

    Deal first cards.

--- src/OpeningDeal.hs
+++ src/OpeningDeal.hs
@@ -4,7 +4,7 @@ import Types
 import Deck
 import Actions  (drawCard)
 import Control.Monad.Trans.State
-import Control.Monad (forM_)
+import Control.Monad (forM)
 
 dealOpeningHands :: State Game ()
 dealOpeningHands = do
@@ -14,7 +14,14 @@ dealOpeningHands = do
     -- for each player, access the active hands
     -- for each active hand, draw a card
     -- return each player with a card added to each active hand keeping the original bet
- 
+    newPlayers <- forM ps $ \p -> do
+        newHandBets <- forM (activeHands p) $ \(h, b) -> do
+            newCard <- drawCard
+            return (h ++ [newCard], b)
+        return p { activeHands = newHandBets }
+    put $ g { players = newPlayers }
+
+    
     
 
 
